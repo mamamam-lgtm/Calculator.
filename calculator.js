5aa81ffb017046f2a5d298bf6b4d3d04cc3894ad
Ianara Nicole Dionisio <ianaranicoledionisio@gmail.com>
2025-10-27 21:22:59 +0800

    Divide Commit

--- calculator.js
+++ calculator.js
@@ -12,9 +12,14 @@ function multiply(a, b){
     return a*b
 }
 
+function divide(a, b){
+    return a/b
+}
+
 let a = 6
 let b = 3
 
 console.log("ADD: ", add(a, b))
 console.log("SUBSTRACT: ", subtract(a, b))
 console.log("MULTIPLY: ", multiply(a, b))
+console.log("DIVIDE: ", divide(a, b))
